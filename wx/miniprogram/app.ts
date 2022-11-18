ee858087465e05b5da69d9d732753721d31e3312
Jack Li <luckydog612@163.com>
2022-11-18 15:49:42 +0800

    获取用户信息

--- wx/miniprogram/app.ts
+++ wx/miniprogram/app.ts
@@ -1,18 +1,48 @@
 // app.ts
-App<IAppOption>({
-  globalData: {},
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
-  },
+import {getSetting, getUserProfile} from "./utils/util";
+
+let resolveUserInfo: (value: (WechatMiniprogram.UserInfo | PromiseLike<WechatMiniprogram.UserInfo>)) => void
+let rejectUserInfo: (reason?: any) => void
+
+App<IAppOption>({
+    globalData: {
+        userInfo: new Promise((resolve, reject) => {
+            resolveUserInfo = resolve
+            rejectUserInfo = reject
+        })
+    },
+    onLaunch() {
+        // 展示本地存储能力
+        const logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+
+        // 登录
+        wx.login({
+            success: res => {
+                console.log('..................')
+                console.log(res)
+                console.log(res.code)
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            },
+        })
+
+        // 获取用户信息
+        getSetting().then(res => {
+            if (res.authSetting['scope.userInfo']) {
+                return getUserProfile()
+            }
+            return undefined
+        }).then(res => {
+            if (!res) {
+                return
+            }
+            resolveUserInfo(res.userInfo)
+            // }).catch(err => reject(err))
+        }).catch(rejectUserInfo)
+    },
+    resolveUserInfo(userInfo: WechatMiniprogram.UserInfo) {
+        resolveUserInfo(userInfo)
+    }
 })
--- wx/miniprogram/utils/util.ts
+++ wx/miniprogram/utils/util.ts
@@ -1,19 +1,42 @@
 export const formatTime = (date: Date) => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
 
-  return (
-    [year, month, day].map(formatNumber).join('/') +
-    ' ' +
-    [hour, minute, second].map(formatNumber).join(':')
-  )
+    return (
+        [year, month, day].map(formatNumber).join('/') +
+        ' ' +
+        [hour, minute, second].map(formatNumber).join(':')
+    )
 }
 
 const formatNumber = (n: number) => {
-  const s = n.toString()
-  return s[1] ? s : '0' + s
+    const s = n.toString()
+    return s[1] ? s : '0' + s
+}
+
+export function getSetting(): Promise<WechatMiniprogram.GetSettingSuccessCallbackResult> {
+    return new Promise((resolve, reject) => {
+        wx.getSetting({
+            // success: res => resolve(res),
+            success: resolve,
+            // fail: err => reject(err)
+            fail: reject,
+        })
+    })
+}
+
+export function getUserProfile(): Promise<WechatMiniprogram.GetUserProfileSuccessCallbackResult> {
+    return new Promise((resolve) => {
+        wx.getUserProfile({
+            desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            success: (res) => {
+                console.log(res)
+                resolve(res)
+            }
+        })
+    })
 }
