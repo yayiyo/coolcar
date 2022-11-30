cdcbc550ab43bd22c2976b7d14a758460c04a300
Jack Li <luckydog612@163.com>
2022-11-30 15:01:29 +0800

    增加地图

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -5,29 +5,69 @@ const app = getApp<IAppOption>()
 
 Page({
     data: {
-        motto: 'Hello World',
-        userInfo: {},
-        hasUserInfo: false,
+        setting: {
+            skew: 0,
+            rotate: 0,
+            showLocation: true,
+            showScale: true,
+            subKey: '',
+            layerStyle: 1,
+            enableZoom: true,
+            enableScroll: true,
+            enableRotate: false,
+            showCompass: false,
+            enable3D: false,
+            enableOverlooking: false,
+            enableSatellite: false,
+            enableTraffic: false,
+        },
+        location: {
+            latitude: 31,
+            longitude: 120,
+        },
+        scale: 10,
+        markers: [
+            {
+                iconPath: "/resources/car.png",
+                id: 0,
+                latitude: 23.0009343,
+                longitude: 113.36299,
+                width: 20,
+                height: 20
+            },
+            {
+                iconPath: "/resources/car.png",
+                id: 1,
+                latitude: 23.6329420,
+                longitude: 114.692635,
+                width: 20,
+                height: 20
+            },
+            {
+                iconPath: "/resources/car.png",
+                id: 2,
+                latitude: 29.47294735,
+                longitude: 113.6329432,
+                width: 20,
+                height: 20
+            }
+        ]
     },
-    // 事件处理函数
-    bindViewTap() {
-        wx.redirectTo({
-            url: '../logs/logs',
-        })
-    },
-    async onLoad() {
-        const userInfo = await app.globalData.userInfo
-        this.setData({
-            userInfo: userInfo,
-            hasUserInfo: true
-        })
-    },
-
-    getUserProfile() {
-        wx.getUserProfile({
-            desc: '展示用户信息',
+    onMyLocationTap() {
+        wx.getLocation({
             success: res => {
-                app.resolveUserInfo(res.userInfo)
+                this.setData({
+                    location: {
+                        latitude: res.latitude,
+                        longitude: res.longitude,
+                    }
+                })
+            },
+            fail: () => {
+                wx.showToast({
+                    title: '没有位置权限',
+                    icon: 'error'
+                })
             }
         })
     }
