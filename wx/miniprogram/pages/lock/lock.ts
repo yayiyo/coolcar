65afcec820e59ae230f72afb2923688ef7f59968
Jack Li <luckydog612@163.com>
2022-12-05 17:40:20 +0800

    开始行程

--- wx/miniprogram/pages/lock/lock.ts
+++ wx/miniprogram/pages/lock/lock.ts
@@ -9,5 +9,34 @@ Page({
       avatarUrl,
     })
     wx.setStorageSync('avatar', avatarUrl)
+  },
+  onUnlockTap() {
+    wx.getLocation({
+      success: loc => {
+        console.log('startting a trip', {
+          location: {
+            latitude: loc.latitude,
+            longitude: loc.longitude,
+          },
+          avatarURL: this.data.avatarUrl,
+        })
+
+        wx.showLoading({
+          title: '开锁中',
+        })
+
+        setTimeout(function () {
+          wx.redirectTo({
+            url: '/pages/driving/driving',
+          })
+        }, 3000)
+      },
+      fail: () => {
+        wx.showToast({
+          title: '没有位置权限',
+          icon: 'error'
+        })
+      }
+    })
   }
 })
