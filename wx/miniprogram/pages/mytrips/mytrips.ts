07a2fe05b7e44afae292f62a132f93425dad83cb
Jack Li <luckydog612@163.com>
2022-12-08 17:53:35 +0800

    行程展示搭建

--- wx/miniprogram/pages/mytrips/mytrips.ts
+++ wx/miniprogram/pages/mytrips/mytrips.ts
@@ -1,5 +1,14 @@
 import { routing } from "../../utils/routing"
 
+interface Trip {
+  id: string
+  start: string
+  end: string
+  duration: string
+  fee: string
+  distance: string
+}
+
 Page({
   data: {
     promotionItems: [
@@ -21,12 +30,41 @@ Page({
       },
     ],
     avatarURL: '',
+    tripsHeight: 0,
+    trips: [] as Trip[],
   },
 
   onLoad() {
+    this.populateTrips()
     const avatarURL = wx.getStorageSync('avatar')
     this.setData({
       avatarURL,
+    })
+  },
+
+  onReady() {
+    wx.createSelectorQuery().select('#heading')
+      .boundingClientRect(res => {
+        this.setData({
+          tripsHeight: wx.getSystemInfoSync().windowHeight - res.height,
+        })
+      }).exec()
+  },
+
+  populateTrips() {
+    const trips: Trip[] = []
+    for (let i = 0; i < 100; i++) {
+      trips.push({
+        id: (10001 + i).toString(),
+        start: '北京',
+        end: '上海',
+        duration: '72时44分55秒',
+        fee: '28888元',
+        distance: '2000KM',
+      })
+    }
+    this.setData({
+      trips,
     })
   },
 
